6e53d27f59223aa909f014663705c88933636d5b
Nya Tyus <nyastyus@gmail.com>
2023-03-07 11:02:04 -0500

    create starter files

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const db = require("./config/connection");
+const { User } = require("./models");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
