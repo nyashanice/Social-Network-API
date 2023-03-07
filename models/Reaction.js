8bbe3ea2d1c8e1d98dce0f226b5d6da3112ad559
Nya Tyus <nyastyus@gmail.com>
2023-03-07 11:47:59 -0500

    create reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,9 +1,19 @@
 // schema only
+const { ObjectId } = require("bson");
+const mongoose = require("mongoose");
 
-// TODO: reactionId (mongoose ObjectId data type, default value is set to new ObjectId)
-// TODO: reactionBody (string, req, 280 char max)
-// TODO: username (string, req)
-// TODO: createdAt (date, default val to current timestamp, getter method to format timestamp on query)
+const reactionSchema = new mongoose.Schema({
+  // TODO: reactionId (mongoose ObjectId data type, default value is set to new ObjectId)
+  reactionId: {type: ObjectId, default: new ObjectId},
+  // TODO: reactionBody (string, req, 280 char max)
+  reactionBody: {type: String, required: true, maxLength: 280},
+  // TODO: username (string, req)
+  username: {type: String, required: true},
+  // TODO: createdAt (date, default val to current timestamp, getter method to format timestamp on query)
+  createdAt: {type: Date, default: Date.now}
+});
+
+
 
 
 // will be used as `reaction` field's subdocument schema in `Thought` model
